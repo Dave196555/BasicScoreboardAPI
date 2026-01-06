8b54fed09c58fb806a86e225d0ad5555645ca486
David Kellam <Davejlightman@gmail.com>
2026-01-06 14:56:10 -0700

    Debug Variables, Logger, Documentation.
    Added XML documentation to the code, along with regions for organization. Also added usage of the Logger<T> for information or debugging purposes.

--- SimpleAPI/SimpleAPI/Program.cs
+++ SimpleAPI/SimpleAPI/Program.cs
@@ -1,4 +1,3 @@
-
 using System.Collections.Concurrent;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,7 +7,17 @@ namespace BasicScoreboardAPI
     {
         private static readonly ConcurrentDictionary<string, int> Leaderboard = new(StringComparer.OrdinalIgnoreCase);
         private static List<ScoreEntry> sortedScores = new List<ScoreEntry>();
-        private static readonly object SortedLock = new();
+        private static DateTime startupTime;
+
+        #region debugVariables
+
+        private static int succesfulPostsSinceStartup = 0;
+        private static int failedPostsSinceStartup = 0;
+        private static int scoreRetrievalsSinceStartup = 0;
+        private static int scoreboardRetrievalsSinceStartup = 0;
+
+        #endregion
+        
         
         public static void Main(string[] args)
         {
@@ -23,39 +32,89 @@ namespace BasicScoreboardAPI
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
+                
+                app.MapGet("/leaderboard/dump", () => { return Results.Ok(Leaderboard);});
+                app.MapGet("/debugStats", SayDeveloperStats);
             }
 
-            app.MapGet("/", SayHello);
-            app.MapGet("/time", SayTime);
+            app.MapGet("/", () => Results.Ok(new
+            {
+                name = "Simple Scoreboard API",
+                status = "running",
+                uptime = GetUpTime()
+            }));
+            app.MapGet("/uptime", SayUpTime);
             app.MapPost("/leaderboard", UpsertScore);
             app.MapGet("/leaderboard/score/{name}", GetScore);
             app.MapGet("/leaderboard/sortByName", ReturnScoreboardByName);
             app.MapGet("/leaderboard/sortByScore", ReturnScoreboardByScore);
-            
-            if(app.Environment.IsDevelopment()) app.MapGet("/leaderboard/dump", () => { return Results.Ok(Leaderboard);});
-            
+
+            startupTime = DateTime.Now;
             app.Run();
         }
 
-        private static string SayHello()
-        {
-            return "Hello World!";
-        }
+        #region Basic Functions
 
-        private static string SayTime()
+        /// <summary>
+        /// Returns Developer statistics collected since the API was started.
+        /// </summary>
+        /// <returns>Returns an IResult. In this case IResult.Ok containing relevant developer debug data.</returns>
+        private static IResult SayDeveloperStats()
         {
-            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            return Results.Ok(new
+            {
+                startTime = startupTime.ToString(),
+                upTime = GetUpTime(),
+                scoreEntryCount = sortedScores.Count,
+                postsSinceStartup = succesfulPostsSinceStartup,
+                invalidPostsSinceStartup = failedPostsSinceStartup,
+                scoreboardRetrievalsSinceStartup = scoreRetrievalsSinceStartup
+            });
         }
+        
+        /// <summary>
+        /// Returns the uptime of the API.
+        /// </summary>
+        private static IResult SayUpTime()
+        {
+            return Results.Ok($"Server Uptime: {GetUpTime()}");
+        }
+        
+        /// <summary>
+        /// Returns a string of the current UpTime in Json format.
+        /// </summary>
+        private static string GetUpTime()
+        {
+            TimeSpan upTime = DateTime.Now - startupTime;
+            return "{" + $"{upTime.Days},{upTime.Hours},{upTime.Minutes},{upTime.Seconds}" + "}";
+        }
+        
+        #endregion
+        
+        #region ScoreboardFunctions
         
         public record ScorePost(string Name, int Score);
         public record ScoreEntry(string Name, int Score);
         public record ScoreDto(string Name, int Score);
 
-        private static IResult UpsertScore([FromBody] ScorePost post)
+        /// <summary>
+        /// Used to Post a score.
+        /// </summary>
+        /// <param name="post">The score to post. Contains a name and a score.</param>
+        /// <returns>
+        /// 400 Bad Request if the name is longer than 64 characters.
+        /// 201 Created If a new score was created.
+        /// 200 OK If the name already existed and the score was updated.
+        /// </returns>
+        private static IResult UpsertScore([FromBody] ScorePost post, ILogger<Program> logger)
         {
             if (post.Name.Length > 64)
             {
-                DebugLog($"Tried to update name {post.Name} but it was too long! It's length was {post.Name.Length}.");
+                logger.LogWarning($"Tried to update name {post.Name} but it was too long! It's length was {post.Name.Length}.",
+                    post.Name,
+                    post.Score);
+                
+                failedPostsSinceStartup++;
                 return Results.BadRequest();
             }
             
@@ -66,37 +125,58 @@ namespace BasicScoreboardAPI
                 addValue: post.Score,
                 updateValueFactory: (_, oldScore) => Math.Max(oldScore, post.Score)
                 );
+            
             sortedScores = ReturnOrderedList();
             
-            
-            DebugLog($"Trying to update or set Name \"{post.Name}\" with value \"{post.Score}\".");
+            logger.LogInformation($"Received Request to Update or set Name \"{post.Name}\" with value \"{post.Score}\".",
+                post.Name, 
+                post.Score);
             
             IResult result = existed ? 
                 Results.Ok(new { post.Name, post.Score, Updated = true }) : 
                 Results.Created($"/leaderboard/{post.Name}", new {post.Name, post.Score, Created = true});
+
+            succesfulPostsSinceStartup++;
             
             return result;
         }
 
-        private static void DebugLog(string inString)
-        {
-            Console.WriteLine($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}]: " + inString);
-        }
-
         public sealed record OutputScore(string name, int score);
         
+        /// <summary>
+        /// Returns the score of the given name.
+        /// </summary>
+        /// <param name="name">The name used to look for the score.</param>
+        /// <returns>
+        /// 200 OK If the score existed and returns the score.
+        /// 404 Not Found If the name is not in the list. 
+        /// </returns>
         private static IResult GetScore(string name)
         {
+            scoreRetrievalsSinceStartup++;
             return Leaderboard.TryGetValue(name, out int score) ? 
                 Results.Ok(new OutputScore(name, score)) : 
                 Results.NotFound(new {Name = name});
         }
 
+        /// <summary>
+        /// Returns the Scoreboard sorted by score.
+        /// </summary>
+        /// <returns>
+        /// 200 OK with the scoreboard sorted by score.
+        /// </returns>
         private static IResult ReturnScoreboardByScore()
         {
+            scoreboardRetrievalsSinceStartup++;
             return Results.Ok(sortedScores);
         }
         
+        /// <summary>
+        /// Returns the Scoreboard sorted by Name.
+        /// </summary>
+        /// <returns>
+        /// 200 OK with the scoreboard sorted by name.
+        /// </returns>
         private static IResult ReturnScoreboardByName()
         {
             var snapshot = Leaderboard;
@@ -108,9 +188,15 @@ namespace BasicScoreboardAPI
                     score = kvp.Value
                 })
                 .ToList();
+
+            scoreboardRetrievalsSinceStartup++;
             return Results.Ok(sorted);
         }
 
+        /// <summary>
+        /// Returns the dictionary as an ordered list.
+        /// </summary>
+        /// <returns>A List of type ScoreEntry sorted in descending order by score.</returns>
         private static List<ScoreEntry> ReturnOrderedList()
         {
             var snapshot = Leaderboard;
@@ -121,5 +207,6 @@ namespace BasicScoreboardAPI
 
             return sorted;
         }
+        #endregion
     }
 }
